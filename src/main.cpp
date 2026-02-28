f5471f8f0a361dc601590b8a2b8aa09e879d2150
lavany108 <lavanya.gla_cs.aiml24@gla.ac.in>
2026-02-28 21:45:41 +0530

    Added docstrings.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+/**
+* @file main.cpp
+ * @brief Arduino sketch for reading temperature and humidity from a DHT11 sensor.
+ *
+ * This sketch uses the DHT library to interface with a DHT11 temperature and
+ * humidity sensor. Readings are printed to the Serial console at 9600 baud.
+ *
+ * Requirements:
+ *  - Arduino core
+ *  - DHT sensor library (https://github.com/adafruit/DHT-sensor-library or equivalent)
+ *
+ * Usage:
+ *  - Connect the DHT11 data pin to the digital pin defined by @ref DHTPIN.
+ *  - Power the sensor with 3.3V/5V and connect ground.
+ *  - Open the Serial Monitor at 9600 baud to view readings.
+ *
+ * @author Lavanya
+ * @date 2026-02-28
+ * @version 1.0
+ */
 #include <Arduino.h>
 #include <DHT.h>
 #define DHTPIN 2

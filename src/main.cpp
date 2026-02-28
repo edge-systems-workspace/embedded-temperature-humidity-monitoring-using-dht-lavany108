2ec73e55dfb122d53d37421137c24e3d3079c27f
lavany108 <lavanya.gla_cs.aiml24@gla.ac.in>
2026-02-28 21:44:03 +0530

    Added logic code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,22 @@
 #include <Arduino.h>
 #include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("DHT11 initialized");
+}
+void loop() {
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        delay(2000);
+        return;
+    }
+
+    delay(2000);
+}
